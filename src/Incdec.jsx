46fcd10245be1e0d3e0b68b71bd0d8bd2ed0d3f5
Manish <manishgoyal6681@gmail.com>
2021-12-21 17:35:08 +0530

    used the material icons and ui

--- src/Incdec.jsx
+++ src/Incdec.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const Incdecrement = () =>{
     const [incr, setinc] = useState(0);
@@ -12,18 +16,25 @@ const Incdecrement = () =>{
             setinc(incr-1);
         } else{
             alert("Sorry, Zero Limit Reach")
-            setinc(0);
         }      
     };   
-
     return(
         <>
         <div className="main_div">
             <div className="center_div">
                 <h1>{incr}</h1>
+                
                 <div className="btn_div">
-                    <button onClick={incnumber}>Increament</button>
-                    <button onClick={decnumber}>Decreament</button>
+                    
+                <Tooltip title="Add">
+                    <Button onClick={incnumber} className="btn_green"><AddIcon /></Button></Tooltip>
+
+                <Tooltip title="Delete">
+                    <Button onClick={decnumber} className="btn_red">
+                        <DeleteIcon/>
+                    </Button>
+                </Tooltip>
+
                 </div>
             </div> 
         </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import Demo from "./Demo";
+
 import Incdecrement from "./Incdec";
 
 ReactDOM.render(
